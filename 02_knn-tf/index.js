56616b1889e78ab378de9583a0ca3562259d9e34
Juan David <juandavid918@gmail.com>
2025-01-28 11:17:04 -0500

    Se ajusta problema para instalar Tensorflow y se ejecuta el proyecto correctamente

--- 02_knn-tf/index.js
+++ 02_knn-tf/index.js
@@ -3,6 +3,20 @@ require("@tensorflow/tfjs-node");
 const tf = require("@tensorflow/tfjs");
 const loadCSV = require("./load-csv");
 
+function knn(features, labels, predictionPoint, k) {
+    return features
+        .sub(predictionPoint) // restar longitudes y latitudes con punto de predicción
+        .pow(2) // elevar al cuadrado
+        .sum(1) // sumar elementos en eje vertical
+        .pow(0.5) // raíz cuadrada
+        .expandDims(1) // poner el mismo shape que en nuestros labels
+        .concat(labels, 1) // concatenar con labels
+        .unstack() // disponer de métodos de JavaScript para arreglos
+        .sort((a, b) => a.get(0) - b.get(0)) // ordenar
+        .slice(0, k) // obtener primeros K elementos
+        .reduce((acc, pair) => acc + pair.get(1), 0) / k; // sumar y calcular promedio
+}
+
 let { features, labels, testFeatures, testLabels } = loadCSV("kc_house_data.csv", {
     shuffle: true,
     splitTest: 10,
@@ -10,5 +24,9 @@ let { features, labels, testFeatures, testLabels } = loadCSV("kc_house_data.csv"
     labelColumns: ["price"]
 });
 
-console.log(testFeatures);
-console.log(testLabels);
+features = tf.tensor(features);
+labels = tf.tensor(labels);
+
+const result = knn(features, labels, tf.tensor(testFeatures[0]), 10);
+
+console.log("Guess", result, testLabels[0][0]);
